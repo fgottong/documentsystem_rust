5328619bd97d93320566e6631f9122e25296ea04
Fabian Gottong <fabian.gottong95@gmail.com>
2025-10-08 20:45:34 +0200

    added document.rs

---
+++ src/document.rs
@@ -0,0 +1,16 @@
+use serde::{Deserialize, Serialize};
+use core::fmt;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct Document{
+    pub title: String, 
+    pub author: String,
+    pub content: String,
+    // created_date: date,
+}
+
+impl fmt::Display for Document {
+    fn fmt(&self,f:&mut fmt::Formatter)-> fmt::Result{
+        write!(f, "({} - {}; {})", self.author, self.title, self.content)
+    }
+}
