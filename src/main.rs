56b1af40a372bc7517cf153f1775c88ed1e4aa08
Fabian Gottong <fabian.gottong95@gmail.com>
2025-10-23 20:59:27 +0200

    add | added files for Category and DocSys too improve data model

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,19 @@
 pub mod apierror;
+pub mod category;
 pub mod document;
+pub mod documentsystem;
 pub mod endpoints;
 pub mod readinglist;
 pub mod router;
 
 use crate::router::create_router;
 
+use crate::documentsystem::DocumentSystem;
+
 #[tokio::main]
 async fn main() {
+    let doc_sys: DocumentSystem = DocumentSystem::new();
+
     let app = create_router();
     let addr = "localhost:7878".to_string();
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
