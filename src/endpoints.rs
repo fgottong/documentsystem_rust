a7d302197189fef3ee984a82178a00d7c8f45a90
Fabian Gottong <fabian.gottong95@gmail.com>
2025-11-02 11:57:16 +0100

    fix | restores server endpoints and router to direct function calls.

--- src/endpoints.rs
+++ src/endpoints.rs
@@ -3,37 +3,21 @@ use crate::readinglist::ReadingList;
 use axum::{Json, response::IntoResponse};
 use serde_json::{Value, json};
 
-pub struct Endpoints {}
-
-pub trait RequestHandler {
-    async fn health_check() -> impl IntoResponse;
-    async fn get_err() -> Result<Json<Value>, ApiError>;
-    async fn get_readinglist() -> Json<Value>;
+/// handler-function//API-Endpoint just to check if server is running
+pub async fn health_check() -> impl IntoResponse {
+    Json(json!({
+            "status":"ok",
+            "message":"Server läuft"
+    }))
 }
 
-impl RequestHandler for Endpoints {
-    /// handler-function//API-Endpoint just to check if server is running
-    async fn health_check() -> impl IntoResponse {
-        Json(json!({
-                "status":"ok",
-                "message":"Server läuft"
-        }))
-    }
-
-    /// Test-Function, that always returns an error.
-    async fn get_err() -> Result<Json<Value>, ApiError> {
-        Err(ApiError::InternalError)
-    }
-
-    /// Returns the current reading list...
-    async fn get_readinglist() -> Json<Value> {
-        let reading_list = ReadingList::create_reading_list(); // TODO: Optimization: create reading_list once and reuse it;
-        Json(reading_list.to_json())
-    }
+/// Test-Function, that always returns an error.
+pub async fn get_err() -> Result<Json<Value>, ApiError> {
+    Err(ApiError::InternalError)
 }
 
-impl Endpoints {
-    pub fn new() -> Self {
-        return Endpoints {};
-    }
+/// Returns the current reading list...
+pub async fn get_readinglist() -> Json<Value> {
+    let reading_list = ReadingList::create_reading_list(); // TODO: Optimization: create reading_list once and reuse it;
+    Json(reading_list.to_json())
 }
--- src/router.rs
+++ src/router.rs
@@ -1,16 +1,9 @@
-use crate::endpoints::{self, Endpoints, RequestHandler};
+use crate::endpoints::*;
 use axum::{Router, routing::get};
-use tokio::net::tcp::ReuniteError;
-
-pub fn preperare_router() -> Router {
-    let request_handler: Endpoints = Endpoints {};
-    return create_router(request_handler);
-}
-
-pub fn create_router(rh: impl RequestHandler) -> Router {
+pub fn create_router() -> Router {
     return Router::new()
         .route("/", get(|| async { "Hello, World!" }))
-        .route("/health", get(|| rh.health_check()))
-        .route("/err", get(|| rh.get_err()))
-        .route("/readinglist", get(|| rh.get_readinglist()));
+        .route("/health", get(health_check))
+        .route("/err", get(get_err))
+        .route("/readinglist", get(get_readinglist));
 }
